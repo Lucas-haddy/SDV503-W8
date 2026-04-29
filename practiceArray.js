7ad2c9a68fbb660926b2a39aaf40b26d97690890
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-04-29 12:20:40 +1200

    created an object array for practice

---
+++ practiceArray.js
@@ -0,0 +1,14 @@
+/* const courses = {
+      id: "SDV503",
+      tutor: "Ali Kahwaji",
+      level: 500,
+      year: 2026,
+      qualifications: ["Certificate", "Diploma", "Bachelor"],
+      inProgress: true,
+}; */
+
+const courses = [
+    { id: "SDV503", tutor: "Ali Kahwaji", level: 500, year: 2026, qualifications: ["Certificate", "Diploma", "Bachelor"], inProgress: true },
+    { id: "COM502", tutor: "Ali Kahwaji", level: 500, year: 2026, qualifications: ["Certificate", "Diploma", "Bachelor"], inProgress: true },
+    { id: "DES502", tutor: "Ali Kahwaji", level: 500, year: 2026, qualifications: ["Certificate", "Diploma", "Bachelor"], inProgress: true },
+];
